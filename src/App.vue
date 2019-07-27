3fcfc19031de04eff694cd8e4146363b8b24048b
chihhung <chihhung@kloudless.com>
2019-07-27 09:54:25 +0800

    feat: install sass, reset

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,17 @@
 <template>
-  <div class="container whole">
-    <div class="row function-area">
-      <div class="col">
-        <CellContainer/>
-      </div>
-      <div class="col">00:00</div>
-      <div class="col">
-        <FoundationContainer/>
-      </div>
-    </div>
-    <div class="row cascade-area">
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-      <div class="col">card</div>
-    </div>
+  <div>
 
   </div>
-
 </template>
 
 <script>
-import CellContainer from './components/CellContainer.vue';
-import FoundationContainer from './components/FoundationContainer';
 
 export default {
   name: 'app',
   components: {
-    FoundationContainer,
-    CellContainer,
   },
 };
 </script>
 
-<style>
-  .whole {
-    background-color: rgb(163, 203, 228);
-  }
-  .function-area {
-    height: 200px;
-  }
-
-  .cascade-area {
-  }
+<style lang="scss">
 </style>
