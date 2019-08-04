2675171aeb63518907134e58459d08d3727afbce
chihhung <chihhung@kloudless.com>
2019-08-04 16:59:04 +0800

    feat: card css

--- src/App.vue
+++ src/App.vue
@@ -28,10 +28,29 @@
       </div>
     </div>
     <div class="cascade-area">
-      <div class="cascade">cascade</div>
-      <div class="cascade">cascade</div>
-      <div class="cascade">cascade</div>
-      <div class="cascade">cascade</div>
+      <div class="cascade">
+        <div class="card spade">
+          <div class="header-container">
+            <div class="number">A</div>
+            <img class="top-right-pip" src="/images/pips/spade.png">
+          </div>
+          <div>
+            <img class="image" src="/images/card-images/mouse.png">
+          </div>
+          <div>
+            <img class="bottom-left-pip" src="/images/pips/spade.png">
+          </div>
+        </div>
+      </div>
+      <div class="cascade">
+        <div class="card heart"></div>
+      </div>
+      <div class="cascade">
+        <div class="card diamond"></div>
+      </div>
+      <div class="cascade">
+        <div class="card clover"></div>
+      </div>
       <div class="cascade">cascade</div>
       <div class="cascade">cascade</div>
       <div class="cascade">cascade</div>
@@ -61,6 +80,8 @@ export default {
   $diamond-border-color: rgb(236, 226, 219);
   $clover-background-color: rgb(186, 154, 97);
   $clover-border-color: rgb(189, 197, 178);
+
+  $pip-width: 16px;
 
   .main {
     background-color: rgb(163, 203, 228);
@@ -98,6 +119,49 @@ export default {
     .cascade-area {
       display: grid;
       grid-template-columns: auto auto auto auto auto auto auto auto;
+      .card {
+        width: $card-width;
+        height: $card-height;
+        border-radius: 13px;
+        display: grid;
+        grid-template-rows: 20px auto 20px;
+        .header-container {
+          display: block;
+        }
+        .number {
+          float: left;
+          margin: 2px 0 0 5px;
+        }
+        .top-right-pip {
+          width: $pip-width;
+          float: right;
+          margin: 5px 5px 0 0;
+        }
+        .bottom-left-pip {
+          width: $pip-width;
+          float: left;
+          margin: 0 0 5px 5px;
+        }
+        .image {
+          width: 70px;
+        }
+      }
+      .spade {
+        background-color: $spade-background-color;
+        border-color: $spade-border-color;
+      }
+      .heart {
+        background-color: $heart-background-color;
+        border-color: $heart-border-color;
+      }
+      .diamond {
+        background-color: $diamond-background-color;
+        border-color: $diamond-border-color;
+      }
+      .clover {
+        background-color: $clover-background-color;
+        border-color: $clover-border-color;
+      }
     }
   }
 </style>
